d9e287ae9d3e2a7ab43c0062c7470b05e5a3ec6c
teedihuni <dhlee132@gmail.com>
2024-08-19 20:36:49 +0900

    solved 2

--- 10828_stack.py
+++ 10828_stack.py
@@ -6,41 +6,32 @@ def preprocess(order, stack):
     if order == 'top':
         if stack:
             num = stack[-1]
-            return stack, num
+            print(num) 
         else:
-            return stack, -1
+            print(-1) 
             
     elif order == 'pop':
         if stack :
             num = stack[-1]
             stack = stack[:-1]
-            return stack, num
+            print(num) 
         else:
-            return stack , -1
+            print(-1)  
             
     elif order == 'size':
-        return stack, len(stack)
+        print(len(stack)) 
         
     elif order == 'empty':
         if stack:
-            return stack, 0
+            print(0) 
         else:
-            return stack, 1
+            print(1) 
             
     else:
         order, num = order.split(' ')
         if order == 'push':
             stack.append(int(num))
-            return stack, False
-
-results = []
+            
 for i in range(order_cnt):
     order = str(input())
-    stack, number = preprocess(order, stack)
-    if number is not False:
-        results.append(number)
-    print('스택 변화' , stack)
-    print('결과 변화', results)
-    
-for result in results:
-    print(result)
+    preprocess(order, stack)
