97085f8569c04272547df968784081b99604d690
teedihuni <dhlee132@gmail.com>
2024-08-23 13:32:29 +0900

    update

--- stack&que/1966.py
+++ stack&que/1966.py
@@ -2,26 +2,26 @@ import sys
 
 n = int(sys.stdin.readline())
 
-for i in range(n):
+for _ in range(n):
     n, m = map(int,sys.stdin.readline().split()) #n은 case 길이, m은 인덱스
     case = [int(k) for k in sys.stdin.readline().split()]
     
-    result = 1
+    count = 1
     while case :
-        if case[0] < max(case):
-            case.append(case.pop(0))
+        if case[0] < max(case): # 가장 큰값과 비교
+            case.append(case.pop(0)) # 내 뒤에 가장 큰값이 있으면 맨뒤로 이동
         
-        else:
+        else: # 내가 가장 큰값일때
             if m == 0 : 
                 break
             
-            case.pop(0)
-            result += 1
+            case.pop(0) # 가장 큰값 빼고
+            count += 1 # 카운트 해주고
             
-        if m > 0 : 
-            m = m - 1  #인덱스 조절
-        else:
+        if m > 0 : # 위에서 .pop() 한번 해준 이후
+            m = m - 1  # 인덱스 조절
+        else: # m 
             m = len(case) -1 
 
-    print(result)
+    print(count)
     
