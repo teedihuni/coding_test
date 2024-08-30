90f43c3ae284d73b91dbe3f45753631d63a6a9d8
teedihuni <dhlee132@gmail.com>
2024-08-30 18:11:25 +0900

    20240829

--- DFS&BFS/BJ_1012.py
+++ DFS&BFS/BJ_1012.py
@@ -1,37 +1,43 @@
 import sys
-sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
+sys.setrecursionlimit(10000000) 
 
-# dfs 정의
-def dfs(x, y):
-    # 상하좌우
-    dx = [0, 0, -1, 1] 
-    dy = [1, -1, 0, 0]
+case_num = int(input())
 
-    # 네 방향 탐색
+
+
+def dfs(x,y): # 함수 정의
+    
+    graph[x][y] = 0 # 방문처리
+    
+    dx = [0,0,1,-1]
+    dy = [1,-1,0,0]
+     
     for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        # 범위 안에 있고 1이면(=배추이면) 지나간것을 -1로 표시하고 주변 탐색
-        if (0 <= nx < m) and (0 <= ny < n) and graph[ny][nx] == 1:
-            graph[ny][nx] = -1
-            dfs(nx, ny)
-
-t = int(input()) # 테스트 케이스의 개수
-for _ in range(t):
-    m, n, k = map(int, input().split()) # 가로, 세로, 배추 개수
-    graph = [[0 for _ in range(m)] for _ in range(n)]
-
-    # 배추 위치 표시
+        new_x = x + dx[i]
+        new_y = y + dy[i]
+        
+        if 0<=new_x<m and 0<=new_y<n: # 범위 설정
+            if graph[new_x][new_y]: # 아직 방문 x 
+                dfs(new_x,new_y)
+                
+# case_num 설정
+for _ in range(case_num):
+    # 초기 입력 세팅
+    m, n, k = map(int,sys.stdin.readline().split())
+    # 그래프 입력
+    graph = [[0]*n for _ in range(m)]
     for _ in range(k):
-        X, Y = map(int, input().split()) 
-        graph[Y][X] = 1 # X, Y 바꿔서 표시해야하는거 주의!
-
-    # 배추 그룹 수(=배추흰지렁이 개수) 세기
+        x,y = map(int,sys.stdin.readline().split())
+        graph[x][y] = 1
+    
+    # count 정의
     count = 0
-    for a in range(m):
-        for b in range(n):
-            if graph[b][a] == 1:
-                dfs(a ,b)
-                count += 1
+    
+    # 조건 설정    
+    for i in range(m):
+        for j in range(n):
+            if graph[i][j] == 1:
+                dfs(i,j)
+                count +=1
+
     print(count)
